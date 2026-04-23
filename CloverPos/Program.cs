ce2e5afb253912c1125f54465329485f6d3cedbe
ravuri-1 <ravuri@bottlecapps.com>
2026-04-23 16:12:10 +0530

    13044 added under upc_sku_code appconfig

--- CloverPos/Program.cs
+++ CloverPos/Program.cs
@@ -18,7 +18,7 @@ namespace CloverPos
                 {
                     try
                     {
-                      /*  if (current.StoreSettings.StoreId == 10488)
+                      /*if (current.StoreSettings.StoreId == 13044)
                         {
                             Console.WriteLine("Fetching_storeid "+current.StoreSettings.StoreId);
                         }
